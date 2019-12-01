aca23373ba7bfa0500066027a72ffa9a4ca85de4
Cassidy Fortner <cassidy.fortner@gmail.com>
2019-11-30 18:35:46 -0600

    making basic api call, added momentjs to get current date to query
    Got the api call working, still need to figure out a better way to get the date instead of hardcoding it right now

--- scripts/main.js
+++ scripts/main.js
@@ -30,11 +30,35 @@ $(document).ready(function () {
 
 
 
-    })
+    });
 
 
     //front end styles//
 
+    function accessNeO(){
+        //call the near earth object api and attain data from it
+        let appID = "8phoKd5HeuFQGjXL2rQjtHLqkeY9a3xlESjPpoGL";
+
+        //get current week from moment.js
+        // let start_date = moment().year() + "-" +  moment().month() + "-" + moment().date();
+        let start_date = "2019-11-30";
+        console.log(start_date);
+        let end_date="";
+        let queryURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date="+ start_date +"&api_key=" + appID;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+        });
+        
+    }
+    accessNeO();
+    console.log(moment().date());
+    console.log(moment().year());
+    console.log(moment().month());
+    console.log(moment().year() + "-" +  moment().month() + "-" + moment().date());
 
 
 
@@ -51,6 +75,4 @@ $(document).ready(function () {
 
 
 
-
-
-})
+});
