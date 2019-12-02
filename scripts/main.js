f8a056e89cdf4bedd738535485bc26382d67581c
Cassidy Fortner <cassidy.fortner@gmail.com>
2019-12-02 15:10:12 -0600

    just merging in changes on master

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
     });
         //front end styles//
 
-<<<<<<< HEAD
 
 
 
@@ -32,7 +31,6 @@ $(document).ready(function () {
 
 
     });
-=======
     //NASA Pic of the Day API Call
     let nasaApiKey = "8phoKd5HeuFQGjXL2rQjtHLqkeY9a3xlESjPpoGL";
     let queryURL = "https://api.nasa.gov/planetary/apod?api_key=" + nasaApiKey;
@@ -48,9 +46,8 @@ $(document).ready(function () {
                 $(".picOfDay").attr("src", response.url)
             }; 
             //$("#picOfDayContainer").children
-        });
+      
     //End of NASA Pic of Day API Call Section
->>>>>>> master
 
 
    
