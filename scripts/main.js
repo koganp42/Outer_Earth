5b61ca9ac3a1f6c15e133d165ad7bc1f6f10d561
Stephen Webb <stephen01webb@gmail.com>
2019-12-02 13:17:17 -0600

    added modal to potd
    added modal and changed

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,16 @@ $(document).ready(function () {
     $(".comet-card").css("display", "none");
     $("#neoCard").on("click", function () {
         $("#neoCard").css("display", "none");
-
         $(".comet-card").css("display", "block").fadeIn();
 
         $('.close-icon').on('click', function () {
             $(this).closest('.card').fadeOut();
             $("#neoCard").css("display", "block");
-
+        })
+    })
+                //POTD Modal
+        $(".info").on("click",function(){
+            $("#myModal").modal("show");
         })
 
         //front end styles//
@@ -30,7 +33,7 @@ $(document).ready(function () {
 
 
 
-    })
+   
 
 
    
