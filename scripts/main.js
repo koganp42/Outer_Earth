1621bca087a67a059b4c2ed28dfd04cdf501eea2
Kogan <koganp42@gmail.com>
2019-12-08 18:11:27 -0600

    Adding the ISS code block from the bad branch

--- scripts/main.js
+++ scripts/main.js
@@ -189,6 +189,109 @@ accessMissionTargets();
     accessNeO();
     //accessing neo api call
 
+    //Start of ISS Functionality Section
+
+    //This function finds the location of the ISS based on the coordinates passed from the geoLocationISS function it's called in.
+    function findIssCountry(issLat, issLong) {
+        let googleMapsApiKey = "AIzaSyC7CPQ1X9wc7M8DGKJf2r1ykN2thMRttiQ";
+        let queryURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + issLat + "," + issLong + "&result_type=country|natural_feature&key=" + googleMapsApiKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            console.log(response);
+            let issCountry = response.results[0].formatted_address;
+            if (issCountry == undefined) {
+                ("#current-country").text("A part of the ocean where Google fears to go.");
+            } else {
+                $("#current-country").text(issCountry);
+            };
+        });
+    }
+
+    //Declaring global-scope variables for the map functions below. 
+    let mymap;
+    let issMarker;
+    let issIcon = L.icon({
+        iconUrl: "https://img.pngio.com/spacecraft-icons-science-mission-directorate-iss-png-300_230.png",
+        iconSize: [38, 95],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76]
+    });
+
+    //This function gets the coordinates of the ISS, creates and/or recreates the ISS map icon based off them, and also passes those coordinates into the findIssCountry function.
+    function geoLocationISS() {
+        let queryURL = "http://api.open-notify.org/iss-now.json";
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            console.log(response);
+            let issLat = parseFloat(response.iss_position.latitude);
+            let issLong = parseFloat(response.iss_position.longitude);
+            console.log(`The ISS's current coordinates: ${issLat}, ${issLong}`);
+            if (issMarker) {
+                issMarker.remove();
+            }
+            issMarker = L.marker([issLat, issLong], { icon: issIcon });
+            issMarker.addTo(mymap).bindPopup("<p>The ISS, a spacecraft laboratory built by the space agencies of the U.S.A., Canada, E.U., Japan, and Russia, orbits the Earth an average of 15.5 times per day at 27,600 km/h. For more information, go click <a href='https://en.wikipedia.org/wiki/International_Space_Station' target='_blank'>here</a>.</p>");
+            mymap.setView([issLat, issLong], 2.3);
+            findIssCountry(issLat, issLong);
+        });
+    }
+
+    //This function inits the map and adds a worldmap tile layer.
+    function initMap() {
+        mymap = L.map('issMap', {
+            zoomControl: false,
+            scrollWheelZoom: false,
+            doubleClickZoom: false
+        });
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox/satellite-v9',
+            accessToken: 'pk.eyJ1Ijoia29nYW5wNDIiLCJhIjoiY2szdzFtcmI0MHMyejNqcGRqcmI2dnZuOSJ9.7YUzCLkvXsTXW0s2L8Nj-Q'
+        }).addTo(mymap);
+    };
+
+    //Invoking the map creator and first call of the ISS icon location, then updating the ISS map location every 15 seconds. 
+    initMap();
+    geoLocationISS();
+    let issInterval = setInterval(geoLocationISS, 15000);
+
+    //The function below will find the next time the ISS will pass by a user's location by getting the user's latitude and longitude and then passing them into the second function.
+    function findUserCoordinates() {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(`The user's current coordinates are: ${position.coords.latitude}, ${position.coords.longitude}`);
+            let userLat = position.coords.latitude;
+            let userLong = position.coords.latitude;
+            findPassTime(userLat, userLong);
+        });
+    };
+    findUserCoordinates();
+    function findPassTime(userLat, userLong) {
+        let queryURL = `http://api.open-notify.org/iss-pass.json?lat=${userLat}&lon=${userLong}`;
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            dataType: "JSONP",
+        }).then(function (response) {
+            console.log(response);
+            let unixTimeStamp = response.response[0].risetime;
+            let passDate = new Date(unixTimeStamp * 1000);
+            let hours = passDate.getHours();
+            let ampm = hours >= 12 ? "pm" : "am";
+            hours = hours % 12;
+            hours = hours ? hours : 12;
+            let minutes = "0" + passDate.getMinutes();
+            let seconds = "0" + passDate.getSeconds();
+            let formattedPassTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + ' ' + ampm;
+            let nextPassDisplayFormat = passDate.toDateString();
+            $("#ISS-pass-time").text(`The ISS will next be over your location at: ${formattedPassTime} on ${nextPassDisplayFormat}.`);
+        });
+    };
+
+    //End of ISS Functionality
     function setSurfaceGravity(){
         $("#weight-on-planet").empty();
         $("#userWeight").val("");
